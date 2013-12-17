151c71ada9138458eda32ec2f1fea118acfa46ab
Robert Knowles <robert.knowles@answerconsulting.com>
2013-12-17 14:37:52 +0000

    improved code

--- src/main/java/answer/king/controller/ItemController.java
+++ src/main/java/answer/king/controller/ItemController.java
@@ -3,28 +3,27 @@ package answer.king.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import answer.king.model.Item;
 import answer.king.service.ItemService;
 
-@Controller
+@RestController
 @RequestMapping("/item")
 public class ItemController {
 	@Autowired
 	private ItemService itemService;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public @ResponseBody List<Item> getAll() {
+	public List<Item> getAll() {
 		return itemService.getAll();
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public @ResponseBody Item create(@RequestBody Item item) {
+	public Item create(@RequestBody Item item) {
 		return itemService.save(item);
 	}
 }
--- src/main/java/answer/king/controller/OrderController.java
+++ src/main/java/answer/king/controller/OrderController.java
@@ -4,41 +4,40 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import answer.king.model.Order;
 import answer.king.model.Reciept;
 import answer.king.service.OrderService;
 
-@Controller
+@RestController
 @RequestMapping("/order")
 public class OrderController {
 	@Autowired
 	private OrderService orderService;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public @ResponseBody List<Order> getAll() {
+	public List<Order> getAll() {
 		return orderService.getAll();
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public @ResponseBody Order create() {
+	public Order create() {
 		Order order = new Order();
 		return orderService.save(order);
 	}
 
 	@RequestMapping(value = "/{id}/addItem/{itemId}", method = RequestMethod.PUT)
-	public @ResponseBody void addItem(@PathVariable("id") Long id, @PathVariable("itemId") Long itemId) {
+	public void addItem(@PathVariable("id") Long id, @PathVariable("itemId") Long itemId) {
 		orderService.addItem(id, itemId);
 	}
 
 	@RequestMapping(value = "/{id}/pay", method = RequestMethod.PUT)
-	public @ResponseBody Reciept pay(@PathVariable("id") Long id, @RequestBody BigDecimal payment) {
+	public Reciept pay(@PathVariable("id") Long id, @RequestBody BigDecimal payment) {
 		return orderService.pay(id, payment);
 	}
 }
