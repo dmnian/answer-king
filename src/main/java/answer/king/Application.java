0b18909980bac004f471874a724547832fa189a0
Josh.Hilland <jdhilland@gmail.com>
2015-08-11 21:50:29 +0100

    integrated swagger2

--- src/main/java/answer/king/Application.java
+++ src/main/java/answer/king/Application.java
@@ -8,8 +8,10 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Component
+@EnableSwagger2
 @SpringBootApplication
 @EnableTransactionManagement
 public class Application extends SpringBootServletInitializer {
